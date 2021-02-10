e1dc1b0730dfc263db75a55daebcd23ed6041163
Dickson <dickson@aigniter.com>
2021-02-11 06:14:39 +0800

    fix: twind setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 import { AppProps } from 'next/app'
 
+import { setup } from '@/utils/twind'
+
 import '../styles/main.css'
+
+setup() // setup twind
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
--- src/utils/twind.ts
+++ src/utils/twind.ts
@@ -1,8 +1,36 @@
 import typography from '@twind/typography'
-import { setup } from 'twind'
+import { setup, strict } from 'twind'
+import type * as Twind from 'twind'
 
-setup({
+// import * as colors from 'twind/colors'
+
+const originalConfig = require('../../tailwind.config.js')
+
+export const tailwindConfig: Twind.Configuration = {
+  ...originalConfig,
+  // theme: {
+  //   // colors,
+  //   extend: {
+  //     screens: {
+  //       xs: '384px',
+  //     },
+  //     colors: {
+  //       ...colors,
+  //     },
+  //   },
+  // },
   plugins: {
     ...typography(),
   },
-})
+}
+
+function _setup() {
+  setup({
+    ...tailwindConfig,
+    ...(process.env.NODE_ENV === 'development' && {
+      mode: strict,
+    }),
+  })
+}
+
+export { _setup as setup }
