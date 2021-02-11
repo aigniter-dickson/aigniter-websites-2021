4e27e5a063911f51a9de786ed2b54e3a9a0809cc
Dickson <dickson@aigniter.com>
2021-02-11 17:43:08 +0800

    fix: remove converted icons

--- src/assets/images/logo/jarvix-capital/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as jarvixCapitalLogoSquircle } from './jarvixCapitalLogoSquircle'
-export { default as jarvixCapitalLogoHort } from './jarvixCapitalLogoHort'
-export { default as jarvixcapitalLogotypeInline } from './jarvixcapitalLogotypeInline'
