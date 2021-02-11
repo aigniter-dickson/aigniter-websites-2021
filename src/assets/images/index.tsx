8b92a4411db654f9460429df8fb96f3496b0cb7b
Dickson <dickson@aigniter.com>
2021-02-11 17:06:36 +0800

    chore: generated components from svg

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,1 @@
+export { default as jarvixInline } from './jarvixInline'
---
+++ src/assets/images/logo/jarvix-capital/index.tsx
@@ -0,0 +1,3 @@
+export { default as jarvixCapitalLogoSquircle } from './jarvixCapitalLogoSquircle'
+export { default as jarvixCapitalLogoHort } from './jarvixCapitalLogoHort'
+export { default as jarvixcapitalLogotypeInline } from './jarvixcapitalLogotypeInline'
