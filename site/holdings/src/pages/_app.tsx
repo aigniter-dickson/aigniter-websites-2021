d1566c231a3aba279869575670a0398893684905
Dickson <dickson@aigniter.com>
2021-02-18 14:40:45 +0800

    remove twind setup

--- site/holdings/src/pages/_app.tsx
+++ site/holdings/src/pages/_app.tsx
@@ -3,13 +3,7 @@ import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps /* , AppContext */ } from 'next/app'
 
-import { setup } from '@/utils/twind'
-
-import '../styles/main.css'
-
 import theme from '../theme'
-
-setup() // setup twind
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
--- site/holdings/src/pages/_document.tsx
+++ site/holdings/src/pages/_document.tsx
@@ -2,26 +2,11 @@ import React from 'react'
 
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-import { tailwindConfig } from '@/utils/twind'
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {process.env.NODE_ENV === 'development' && (
-            <>
-              <script type="module" src="https://cdn.skypack.dev/twind/shim" />
-              <script
-                type="twind-config"
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: JSON.stringify(tailwindConfig), // eslint-disable-line @typescript-eslint/naming-convention
-                }}
-              />
-            </>
-          )}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
